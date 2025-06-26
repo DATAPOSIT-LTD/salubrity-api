c9492751c7b3eef0757b4a04a743853d1d616d54
hermitex <tiberiusmairura.dev@gmail.com>
2025-06-26 18:58:45 +0300

    🔒 Add JWT Bearer security definition to Swagger for protected endpoints

--- Salubrity.Api/Controllers/Auth/AuthController.cs
+++ Salubrity.Api/Controllers/Auth/AuthController.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Salubrity.Api.Controllers.Common;
 using Salubrity.Application.DTOs.Auth;
 using Salubrity.Application.Interfaces.Services.Auth;
 using Salubrity.Shared.Responses;
-using Salubrity.Api.Controllers.Common;
 
 namespace Salubrity.Api.Controllers.Auth;
 
@@ -39,7 +40,7 @@ public class AuthController : BaseController
         return Success(result, "Login successful.");
     }
 
-
+    [Authorize]
     [HttpGet("me")]
     [ProducesResponseType(typeof(ApiResponse<MeResponseDto>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ApiResponse<string>), StatusCodes.Status401Unauthorized)]
@@ -49,6 +50,18 @@ public class AuthController : BaseController
         var result = await _authService.GetMeAsync(userId);
         return Success(result);
     }
+
+    [Authorize]
+    [HttpGet("me/debug")]
+    public IActionResult MeDebug()
+    {
+        return Ok(new
+        {
+            IsAuthenticated = User.Identity?.IsAuthenticated,
+            Claims = User.Claims.Select(c => new { c.Type, c.Value })
+        });
+    }
+
 
 
     [HttpPost("refresh-token")]

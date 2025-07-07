7973afe8bed6d63d6f982b9f8e8362020cfacf43
hermitex <tiberiusmairura.dev@gmail.com>
2025-07-08 02:32:31 +0300

    fix(health-camps): add missing AutoMapper mappings for HealthCampPackageItem DTOs

--- Salubrity.Application/Mappings/HealthCampProfile.cs
+++ Salubrity.Application/Mappings/HealthCampProfile.cs
@@ -1,8 +1,9 @@
-// File: Salubrity.Application/Mappings/HealthCampProfile.cs
+﻿// File: Salubrity.Application/Mappings/HealthCampProfile.cs
 
 using AutoMapper;
 using Salubrity.Application.DTOs.HealthCamps;
 using Salubrity.Domain.Entities.HealthCamps;
+using Salubrity.Domain.Entities.HealthcareServices;
 
 namespace Salubrity.Application.Mappings;
 
@@ -10,12 +11,19 @@ public class HealthCampProfile : Profile
 {
     public HealthCampProfile()
     {
+        // Create DTO → Entity
         CreateMap<CreateHealthCampDto, HealthCamp>()
             .ForMember(dest => dest.PackageItems, opt => opt.Ignore())
             .ForMember(dest => dest.ServiceAssignments, opt => opt.Ignore())
             .ForMember(dest => dest.Organization, opt => opt.Ignore());
 
+        // Entity → DTO
         CreateMap<HealthCamp, HealthCampDto>()
             .ForMember(dest => dest.OrganizationName, opt => opt.MapFrom(src => src.Organization.BusinessName));
+
+        // Map package items (missing earlier)
+        CreateMap<HealthCampPackageItem, HealthCampPackageItemDto>();
+        CreateMap<CreateHealthCampPackageItemDto, HealthCampPackageItem>();
+        CreateMap<UpdateHealthCampPackageItemDto, HealthCampPackageItem>();
     }
 }

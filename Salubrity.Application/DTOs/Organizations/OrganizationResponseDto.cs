9f92b1d10c042d2f91c0c8431fc21cab88c8616b
hermitex <tiberiusmairura.dev@gmail.com>
2025-07-11 09:04:55 +0300

    Add therapist identity preferences and healthcare benefits lookup tables

--- Salubrity.Application/DTOs/Organizations/OrganizationResponseDto.cs
+++ Salubrity.Application/DTOs/Organizations/OrganizationResponseDto.cs
@@ -6,6 +6,7 @@ namespace Salubrity.Application.DTOs.Organizations
         public string BusinessName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Phone { get; set; } = null!;
+        public string Location { get; set; } = null!;
         public string? ClientLogoPath { get; set; }
         public Guid? ContactPersonId { get; set; }
         public Guid? StatusId { get; set; }

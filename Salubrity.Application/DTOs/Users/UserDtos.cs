aae9cacc30d4b7d518c6e531fdb5ed35ec8a2144
hermitex <tiberiusmairura.dev@gmail.com>
2025-07-25 10:59:09 +0300

    feat: add generic lookup controllers and services for gender, job title, department, and language

--- Salubrity.Application/DTOs/Users/UserDtos.cs
+++ Salubrity.Application/DTOs/Users/UserDtos.cs
@@ -25,7 +25,7 @@ public class UserResponse
 public class UserCreateRequest
 {
     public string Email { get; set; } = null!;
-    public string? Password { get; set; } = null!;
+    public string? Password { get; set; } = "Welcome123!"; 
     public string FirstName { get; set; } = null!;
     public string? MiddleName { get; set; }
     public DateTime? DateOfBirth { get; set; }

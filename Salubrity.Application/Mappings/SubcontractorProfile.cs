65643d6ecbfd9bb2802795d413f33344f5228f58
tiberiusmairura <tiberius.mairura@dataposit.co.ke>
2025-08-07 09:38:34 +0300

    Add GetByNameAsync method to IndustryRepository with AutoMapper support

---
+++ Salubrity.Application/Mappings/SubcontractorProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using Salubrity.Application.DTOs.Subcontractor;
+using Salubrity.Domain.Entities.Subcontractor;
+
+namespace Salubrity.Application.Mappings;
+
+public class SubcontractorProfile : Profile
+{
+    public SubcontractorProfile()
+    {
+        CreateMap<Subcontractor, SubcontractorDto>();
+        CreateMap<CreateSubcontractorDto, Subcontractor>();
+        CreateMap<UpdateSubcontractorDto, Subcontractor>();
+    }
+}

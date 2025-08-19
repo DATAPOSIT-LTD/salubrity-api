4d2016387284d466f9fc1d09af5be15dc8c2c4bd
tiberiusmairura <tiberius.mairura@dataposit.co.ke>
2025-08-19 19:28:41 +0300

    Camp creation: auto-enroll all active organization employees as HealthCampParticipants; add IEmployeeReadRepository + infra impl; wire DI; keep service orchestration-only

--- Salubrity.Application/DTOs/Subcontractor/SubcontractorRoleDto.cs
+++ Salubrity.Application/DTOs/Subcontractor/SubcontractorRoleDto.cs
@@ -2,11 +2,9 @@ namespace Salubrity.Application.DTOs.Subcontractor;
 
 public class SubcontractorRoleDto
 {
-    public Guid Id { get; set; }
-
+    public Guid RoleId { get; set; }
     public Guid SubcontractorId { get; set; }
-
     public string RoleName { get; set; } = default!;
-
     public string? Description { get; set; }
 }
+
--- Salubrity.Application/Mappings/SubcontractorProfile.cs
+++ Salubrity.Application/Mappings/SubcontractorProfile.cs
@@ -1,7 +1,6 @@
 using AutoMapper;
 using Salubrity.Application.DTOs.Subcontractor;
 using Salubrity.Domain.Entities.Subcontractor;
-using Salubrity.Domain.Entities.Lookup;
 
 namespace Salubrity.Application.Mappings;
 
@@ -9,18 +8,18 @@ public class SubcontractorProfile : Profile
 {
     public SubcontractorProfile()
     {
-        // Subcontractor -> DTO
+        // Main Subcontractor → DTO
         CreateMap<Subcontractor, SubcontractorDto>()
             .ForMember(d => d.FullName, o => o.MapFrom(s => s.User.FullName))
             .ForMember(d => d.Email, o => o.MapFrom(s => s.User.Email))
-            .ForMember(d => d.PhoneNumber, o => o.MapFrom(s => s.User.Phone)) // change to .PhoneNumber if your User has that prop
+            .ForMember(d => d.PhoneNumber, o => o.MapFrom(s => s.User.Phone)) // Use .PhoneNumber if that's the property
             .ForMember(d => d.IndustryName, o => o.MapFrom(s => s.Industry.Name))
             .ForMember(d => d.StatusName, o => o.MapFrom(s => s.Status.Name))
             .ForMember(d => d.Specialties, o => o.MapFrom(s => s.Specialties))
-            .ForMember(d => d.Roles, o => o.MapFrom(s => s.RoleAssignments))  // uses assignment -> dto map
+            .ForMember(d => d.Roles, o => o.MapFrom(s => s.RoleAssignments))
             .ForMember(d => d.CampAssignmentCount, o => o.MapFrom(s => s.CampAssignments.Count));
 
-        // Create/Update
+        // Create and Update DTO → Entity
         CreateMap<CreateSubcontractorDto, Subcontractor>();
         CreateMap<UpdateSubcontractorDto, Subcontractor>();
 
@@ -29,13 +28,11 @@ public class SubcontractorProfile : Profile
             .ForMember(d => d.ServiceName, o => o.MapFrom(s => s.Service.Name));
         CreateMap<SubcontractorSpecialtyDto, SubcontractorSpecialty>();
 
-        // ROLES: map from the assignment join to the role dto
+        // Role assignments → Role DTO (profession)
         CreateMap<SubcontractorRoleAssignment, SubcontractorRoleDto>()
-            .ForMember(d => d.Id, o => o.MapFrom(s => s.Id)) // assignment id; switch to s.SubcontractorRole.Id if you prefer role id
+            .ForMember(d => d.RoleId, o => o.MapFrom(s => s.Id)) // Assignment ID (not role ID)
             .ForMember(d => d.SubcontractorId, o => o.MapFrom(s => s.SubcontractorId))
             .ForMember(d => d.RoleName, o => o.MapFrom(s => s.SubcontractorRole.Name))
             .ForMember(d => d.Description, o => o.MapFrom(s => s.SubcontractorRole.Description));
-
-
     }
 }

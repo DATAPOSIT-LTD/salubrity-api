7b8c627b63cf99c47a088fbbc71f7f0c16d5209b
tiberiusmairura <tiberius.mairura@dataposit.co.ke>
2025-12-01 13:30:12 +0300

    HealthCampPackageServices

--- Salubrity.Infrastructure/Migrations/20251022100334_UpdateEmployeeRelationships.cs
+++ Salubrity.Infrastructure/Migrations/20251022100334_UpdateEmployeeRelationships.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿// using System;
+// using Microsoft.EntityFrameworkCore.Migrations;
 
-#nullable disable
+// #nullable disable
 
-namespace Salubrity.Infrastructure.Migrations
-{
-    /// <inheritdoc />
-    public partial class UpdateEmployeeRelationships : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "HealthCampPackageServices");
-        }
+// namespace Salubrity.Infrastructure.Migrations
+// {
+//     /// <inheritdoc />
+//     public partial class UpdateEmployeeRelationships : Migration
+//     {
+//         /// <inheritdoc />
+//         protected override void Up(MigrationBuilder migrationBuilder)
+//         {
+//             migrationBuilder.DropTable(
+//                 name: "HealthCampPackageServices");
+//         }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "HealthCampPackageServices",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    HealthCampPackageId = table.Column<Guid>(type: "uuid", nullable: false),
-                    ServiceId = table.Column<Guid>(type: "uuid", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_HealthCampPackageServices", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_HealthCampPackageServices_HealthCampPackages_HealthCampPack~",
-                        column: x => x.HealthCampPackageId,
-                        principalTable: "HealthCampPackages",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+//         /// <inheritdoc />
+//         protected override void Down(MigrationBuilder migrationBuilder)
+//         {
+//             migrationBuilder.CreateTable(
+//                 name: "HealthCampPackageServices",
+//                 columns: table => new
+//                 {
+//                     Id = table.Column<Guid>(type: "uuid", nullable: false),
+//                     HealthCampPackageId = table.Column<Guid>(type: "uuid", nullable: false),
+//                     ServiceId = table.Column<Guid>(type: "uuid", nullable: false)
+//                 },
+//                 constraints: table =>
+//                 {
+//                     table.PrimaryKey("PK_HealthCampPackageServices", x => x.Id);
+//                     table.ForeignKey(
+//                         name: "FK_HealthCampPackageServices_HealthCampPackages_HealthCampPack~",
+//                         column: x => x.HealthCampPackageId,
+//                         principalTable: "HealthCampPackages",
+//                         principalColumn: "Id",
+//                         onDelete: ReferentialAction.Cascade);
+//                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_HealthCampPackageServices_HealthCampPackageId",
-                table: "HealthCampPackageServices",
-                column: "HealthCampPackageId");
-        }
-    }
-}
+//             migrationBuilder.CreateIndex(
+//                 name: "IX_HealthCampPackageServices_HealthCampPackageId",
+//                 table: "HealthCampPackageServices",
+//                 column: "HealthCampPackageId");
+//         }
+//     }
+// }

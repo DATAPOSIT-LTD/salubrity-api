7418213c186a103b6d598ec9de9496c9632db032
tiberiusmairura <tiberius.mairura@dataposit.co.ke>
2025-12-16 11:03:49 +0300

    fix rsa key

--- Salubrity.Shared/Security/Config/JwtSettings.cs
+++ Salubrity.Shared/Security/Config/JwtSettings.cs
@@ -2,8 +2,8 @@ namespace Salubrity.Shared.Security.Config
 {
     public class JwtSettings
     {
-        public string Issuer { get; set; } = default!;
-        public string Audience { get; set; } = default!;
+        public string Issuer { get; set; } = "Salubrity";
+        public string Audience { get; set; } = "SalubrityClient";
 
         // REQUIRED for symmetric JWT (HMAC)
         public string Secret { get; set; } = "PnKsjeXK7BkNABRCa2pb8f+3MkyDnQZxVKCBvAp8AS6bm7LyPSps66+S7JJrYMWXNlz5baLybKjVub5bpx8Ykg==";
